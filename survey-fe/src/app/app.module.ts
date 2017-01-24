f6f2f060cc3309b43808c30de30816ff4eb56035
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2017-01-24 11:40:51 +0200

    Angular material added

--- survey-fe/src/app/app.module.ts
+++ survey-fe/src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
